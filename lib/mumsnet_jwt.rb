826788565a88923b51b1c25a6635b5978bdcfc37
Murray Catto <murraycatto@live.co.za>
2018-07-23 15:11:15 +0100

    update Env fields

--- lib/mumsnet_jwt.rb
+++ lib/mumsnet_jwt.rb
@@ -3,19 +3,19 @@ module MumsnetJWT
     require 'jwt'
     DEFAULT_EXP = Time.now.to_i + 60 * 60 * 24
     def tokenify(extra_payload: {})
-      payload = { access_token: ENV['API_ACCESS_TOKEN'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
-      token = JWT.encode payload, ENV['JWT_SECERET'], 'HS256'
+      payload = { jwt_check: ENV['JWT_CHECK'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
+      token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
       token
     end
 
     def check_token(token)
-      decode_token(token: token, key: 'access_token') == ENV['API_ACCESS_TOKEN']
+      decode_token(token: token, key: 'jwt_check') == ENV['JWT_CHECK']
     rescue StandardError
       false
     end
 
     def decode_token(token:, key: nil)
-      decoded_token = JWT.decode(token, ENV['JWT_SECERET'], true, algorithm: 'HS256')
+      decoded_token = JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256')
       if key
         decoded_token[0][key]
       else
