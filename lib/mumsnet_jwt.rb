3c601b7cdd66ef8dd184aa724560a7c2b65f4f56
Murray Catto <murraycatto@live.co.za>
2018-07-26 17:24:48 +0100

    update gem file to use client id

--- lib/mumsnet_jwt.rb
+++ lib/mumsnet_jwt.rb
@@ -1,20 +1,25 @@
 module MumsnetJWT
   class << self
     require 'jwt'
+    require 'json'
+    require 'base64'
+
     DEFAULT_EXP = Time.now.to_i + 60 * 60 * 24
     def tokenify(extra_payload: {})
-      payload = { jwt_client_id: ENV['JWT_CLIENT_ID'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
+      payload = { client_id: ENV['JWT_CLIENT_ID'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
       JWT.encode payload, jwt_secret, 'HS256' unless jwt_secret.nil?
     end
 
-    def check_token(token, jwt_client_id = ENV['JWT_CLIENT_ID'])
-      decode_token(token: token, key: 'jwt_client_id') == jwt_client_id
+    def check_token(token)
+      client_id = client_id_from_token(token)
+      decode_token(token: token, key: 'client_id', client_id: client_id) == client_id
     rescue StandardError
       false
     end
 
-    def decode_token(token:, key: nil, jwt_client_id: ENV['JWT_CLIENT_ID'])
-      decoded_token = JWT.decode(token, jwt_secret(jwt_client_id), true, algorithm: 'HS256') unless jwt_secret(jwt_client_id).nil?
+    def decode_token(token:, key: nil)
+      client_id = client_id_from_token(token)
+      decoded_token = JWT.decode(token, jwt_secret(client_id), true, algorithm: 'HS256') unless jwt_secret(client_id).nil?
       if key
         decoded_token[0][key]
       else
@@ -24,10 +29,14 @@ module MumsnetJWT
       nil
     end
 
-    def jwt_secret(jwt_client_id = ENV['JWT_CLIENT_ID'])
+    def client_id_from_token(token)
+      JSON.parse(Base64.decode64(token.split('.')[1]))['client_id']
+    end
+
+    def jwt_secret(client_id = ENV['JWT_CLIENT_ID'])
       jwt_secret = nil
       JSON.parse(ENV['JWT_SECRETS']).each do |item|
-        jwt_secret = item['secret'] if item['client_id'] == jwt_client_id
+        jwt_secret = item['secret'] if item['client_id'] == client_id
       end
       jwt_secret
     end
