8b7ff1204ef324d25ab6209535c64f98b1524998
Murray Catto <murraycatto@live.co.za>
2018-07-26 14:23:16 +0100

    update to multi client_id based auth

--- lib/mumsnet_jwt.rb
+++ lib/mumsnet_jwt.rb
@@ -3,19 +3,18 @@ module MumsnetJWT
     require 'jwt'
     DEFAULT_EXP = Time.now.to_i + 60 * 60 * 24
     def tokenify(extra_payload: {})
-      payload = { jwt_check: ENV['JWT_CHECK'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
-      token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
-      token
+      payload = { jwt_client_id: ENV['JWT_CLIENT_ID'], iss: ENV['JWT_ISSUER'], exp: DEFAULT_EXP }.merge(extra_payload)
+      JWT.encode payload, jwt_secret, 'HS256' unless jwt_secret.nil?
     end
 
     def check_token(token)
-      decode_token(token: token, key: 'jwt_check') == ENV['JWT_CHECK']
+      decode_token(token: token, key: 'client_id') == ENV['JWT_CLIENT_ID']
     rescue StandardError
       false
     end
 
     def decode_token(token:, key: nil)
-      decoded_token = JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256')
+      decoded_token = JWT.decode(token, jwt_secret, true, algorithm: 'HS256') unless jwt_secret.nil?
       if key
         decoded_token[0][key]
       else
@@ -23,6 +22,14 @@ module MumsnetJWT
       end
     rescue StandardError
       nil
+    end
+
+    def jwt_secret
+      jwt_secret = nil
+      JSON.parse(ENV['JWT_SECRETS']).each do |item|
+        jwt_secret = item['secret'] if item['client_id'] == ENV['JWT_CLIENT_ID']
+      end
+      jwt_secret
     end
   end
 end
